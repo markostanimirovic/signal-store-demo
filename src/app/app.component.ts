d9d15b7737cc9a05b137802c5342c77b39656b53
markostanimirovic <markostanimirovic95@gmail.com>
2024-03-06 00:09:21 +0100

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'ngrx-root',
   standalone: true,
+  template: 'app',
   imports: [],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'signal-store-demo';
-}
+export class AppComponent {}
